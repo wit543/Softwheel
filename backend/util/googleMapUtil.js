ea5e0579939331de033e4ec681b0b5d2d23455e0
wit543 <norawit543@hotmail.com>
2016-11-07 10:46:57 +0700

    Update googleMapUtil.js

--- backend/util/googleMapUtil.js
+++ backend/util/googleMapUtil.js
@@ -3,14 +3,14 @@
  */
 (function () {
 var http = require('http');
-var options = {
+var google-map-get-info-from-lat-long = {
     host: 'maps.googleapis.com',
     port: 80,
     path: '/maps/api/geocode/json?latlng=13.8416305,100.568995&sensor=false',
     method: 'GET'
 };
 
-http.request(options, function(res) {
+http.request(google-map-get-info-from-lat-long, function(res) {
     console.log(res)
 
 }
