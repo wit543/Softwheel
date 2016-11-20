ece186e3529c23608b5933fba69c30893c20285a
Norawit <wit543@gmail.com>
2016-11-20 17:47:38 +0700

    remove unsure module. move from routemanager to util

--- backend/util/googleMapUtil.js
+++ backend/util/googleMapUtil.js
@@ -1,25 +1,33 @@
-/**
- * Created by wit on 10/31/2016.
- */
 (function () {
-    var http = require('http');
-    var options = {
-        host: 'maps.googleapis.com',
-        port: 80,
-        path: '/maps/api/geocode/json?latlng=13.8416305,100.568995&sensor=false',
-        method: 'GET'
-    };
 
-    module.exports = function (net) {
-        http.request(options, function(res) {
-            console.log(res)
-
-        }).end();
+    class google_map{
+        constructor(http){
+            this.http=http;
+            this.path= '/maps/api/geocode/json?latlng=?{this.lat},${this.lng}&sensor=false';
+            this.options= {
+                host: 'maps.googleapis.com',
+                port: 80,
+                method: 'GET'
+            };
+        }
+        get_location_latlng(lat,lng,callback){
+            this.lat = lat;
+            this.lng = lng
+            this.options["path"]=this.path;
+            console.log(this.options);
+            this.http.request(this.options, function(res) {
+                var str = '';
+                res.on('data',function (chunk) {
+                    console.log(chunk);
+                    str+=chunk;
+                });
+                res.on('end',function () {
+                    callback(JSON.parse(str));
+                })
+            }).end();
+        }
+    }
+    module.exports = function (http) {
+        return new google_map(http)
     };
 })();
-
-
-
-
-
-
