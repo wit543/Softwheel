07d82abecde7944c71e6dd376afa5671be142aa6
Norawit <wit543@gmail.com>
2016-11-21 12:55:04 +0700

    add cors

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,24 @@
 (function () {
     'use strict';
     const express = require("express");
+    const cors = require('cors');
     const app = express();
     const bodyParser = require("body-parser");
     const morgan = require("morgan");
     var path = require('path');
     const port = process.env.PORT || 8888;
+
     app.use(express.static(__dirname+'/public'))
     app.use(bodyParser.urlencoded({extended:false}));
     app.use(bodyParser.json());
     app.use(function (req,res,next) {
-        // res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
         res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, project_id, Authorization, o-Requested-With");
         next();
     });
+    app.use(cors());
 //    "test": "nodemon index.js --exec babel-node --presets es2015,stage-0",
 
 
