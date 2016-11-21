12080da5494a874fff9b56671bff5d2849f23573
Norawit <wit543@gmail.com>
2016-11-21 12:48:27 +0700

    Access-Control-Allow-Origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@
     app.use(bodyParser.urlencoded({extended:false}));
     app.use(bodyParser.json());
     app.use(function (req,res,next) {
-        res.header("Access-Control-Allow-Origin", "*");
+        // res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
         res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, project_id, Authorization, o-Requested-With");
         next();
