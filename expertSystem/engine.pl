d69540255606b8795631676b3bbe8a10cd5e7510
no5013 <pongsakorns@gmail.com>
2016-11-24 18:14:09 +0700

    Update engine.pl

--- expertSystem/engine.pl
+++ expertSystem/engine.pl
@@ -4,15 +4,30 @@ photo_sensitive("RD1").
 grow_well("Chiang_Mai","Chom_Thong","Khuang_Pao","RD6","IN_SEASON").
 grow_well("Chiang_Mai","Chom_Thong","Khuang_Pao","Jasmine105","IN_SEASON").
 grow_well("Chiang_Mai","Chom_Thong","Khuang_Pao","RD44","OFF_SEASON").
-growing_duration("RD1","GROW1",100).
-growing_duration("RD2","GROW1",90).
+growing_duration("RD1","indirect_seeding",100).
+growing_duration("RD2","indirect_seeding",90).
 growing_duration("RD6","indirect_seeding",112).
 growing_duration("Jasmine105","indirect_seeding",130).
 green("Chiang_Mai",date(2016,5,1),date(2016,10,15)).
+red("Chiang_Mai","Chom_Thong","Khuang_Pao",date(2016,1,1),date(2016,4,30)).
 history_rainning("TEST").
 forecast_rainning("TEST").
 rice("RD1").
 rice("RD5").
+province("Chiang_Mai").
+
+grow_well(P1,A1,T1,R1,S1):-
+   rice(R1).
+
+irrigation(P1,A1):-
+   province(P1).
+
+photo_sensitive(R1):-
+   rice(R1).
+
+growing_duration(P1,R1,S1):-
+   province(P1),
+   rice(R1).
 
 can_growing(P1,"indirect_seeding"):-
    history_rainning(P1),
@@ -27,20 +42,6 @@ is_in_season(P1,_, PMONTH, SEASON):-
  SEASON = "IN_SEASON";
  SEASON = "OFF_SEASON".
 
-  
-recommend(P1,A1,T1, R1, G1, PDAY, PMONTH, PYEAR, HDAY, HMONTH, HYEAR):-
-  is_in_season(P1,PDAY,PMONTH,SEASON),
-  recommend_place_rice(P1,A1,T1,R1,SEASON),
-  recommend_place_growingmethod(P1,A1,G1),
-  recommend_rice_season(R1,SEASON),
-  growing_duration(R1,G1,DURATION),
-  DATE = date(PYEAR,PMONTH,PDAY,0,0,0,0,-,-),
-  add_date(DATE,DURATION, HDATE),
-  is_green(P1,HDATE),
-  date_time_value(day,HDATE, HDAY),
-  date_time_value(month,HDATE, HMONTH),
-  date_time_value(year,HDATE, HYEAR).
-
 recommendP(P1,A1,T1, R1, G1, PDAY, PMONTH, PYEAR):-
   is_in_season(P1,PDAY,PMONTH,SEASON),
   recommend_place_rice(P1,A1,T1,R1,SEASON),
@@ -48,7 +49,7 @@ recommendP(P1,A1,T1, R1, G1, PDAY, PMONTH, PYEAR):-
   recommend_rice_season(R1,SEASON),
   PLANTINGDATE = date(PYEAR,PMONTH,PDAY,0,0,0,0,-,-),
   harvest_date(R1,G1,PLANTINGDATE,HDAY,HMONTH,HYEAR),
-  is_green(P1,date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-)).
+  not(is_red(P1,A1,T1,date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-))).
 
 recommendH(P1,A1,T1, R1, G1, HDAY, HMONTH, HYEAR):-
   HARVESTINGDATE = date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-),
@@ -57,7 +58,7 @@ recommendH(P1,A1,T1, R1, G1, HDAY, HMONTH, HYEAR):-
   recommend_place_rice(P1,A1,T1,R1,SEASON),
   recommend_place_growingmethod(P1,A1,G1),
   recommend_rice_season(R1,SEASON),
-  is_green(P1,date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-)).
+  not(is_red(P1,A1,T1,date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-))).
 
 harvest_date(R1,G1,PD1,HDAY, HMONTH, HYEAR):-
   growing_duration(R1,G1,DURATION),
@@ -89,6 +90,10 @@ recommend_rice_season(R1,IN_SEASON):-
 is_green(P1, DATE):-
     green(P1,STARTGREEN,ENDGREEN),
     between_date(STARTGREEN,ENDGREEN,DATE).
+
+is_red(P1, A1, T1, DATE):-
+    red(P1, A1, T1,STARTRED,ENDRED),
+    between_date(STARTRED,ENDRED,DATE).
 
 earlier(DATE1, DATE2) :- 
     date_time_stamp(DATE1, STAMP1),
@@ -124,15 +129,35 @@ simple(P1,A1,T1,R1,G1,SEASON,STARTDAY,STARTMONTH,ENDDAY,ENDMONTH):-
   date_time_value(day, ENDDATE, ENDDAY),
   date_time_value(month, ENDDATE, ENDMONTH).
 
-ex_recommend_place_rice(P1,A1,T1,R1,PDAY,PMONTH):-
+ex_recommendP_place_rice(P1,A1,T1, R1, G1, PDAY, PMONTH, PYEAR):-
+  recommend_place_rice(P1,A1,T1,R1,SEASON).
+
+ex_recommendP_place_growingmethod(P1,A1,T1, R1, G1, PDAY, PMONTH, PYEAR):-
+  recommend_place_growingmethod(P1,A1,G1).
+
+ex_recommendP_rice_season(P1,A1,T1, R1, G1, PDAY, PMONTH, PYEAR):-
   is_in_season(P1,PDAY,PMONTH,SEASON),
-  grow_well(P1,A1,T1,R1,SEASON).
-  
-ex_recommend_rice_season(R1,P1,PDAY,PMONTH):-
-  rice(R1),
-  photo_sensitive(R1);
+  recommend_rice_season(R1,SEASON).
+
+ex_recommendP_harvesting_date(P1,A1,T1, R1, G1, PDAY, PMONTH, PYEAR):-
+  PLANTINGDATE = date(PYEAR,PMONTH,PDAY,0,0,0,0,-,-),
+  harvest_date(R1,G1,PLANTINGDATE,HDAY,HMONTH,HYEAR),
+  not(is_red(P1,A1,T1,date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-))).
+
+ex_recommendH_place_rice(P1,A1,T1, R1, G1, HDAY, HMONTH, HYEAR):-
+  HARVESTINGDATE = date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-),
+  planting_date(R1,G1,HARVESTINGDATE,PDAY,PMONTH,PYEAR),
   is_in_season(P1,PDAY,PMONTH,SEASON),
-  rice(R1),
-  not(photo_sensitive(R1)),
-  SEASON == "IN_SEASON".
-  
+  recommend_place_rice(P1,A1,T1,R1,SEASON).
+
+ex_recommendH_place_growingmethod(P1,A1,T1, R1, G1, HDAY, HMONTH, HYEAR):-
+  recommend_place_growingmethod(P1,A1,G1).
+
+ex_recommendH_place_rice_season(P1,A1,T1, R1, G1, HDAY, HMONTH, HYEAR):-
+  HARVESTINGDATE = date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-),
+  planting_date(R1,G1,HARVESTINGDATE,PDAY,PMONTH,PYEAR),
+  is_in_season(P1,PDAY,PMONTH,SEASON),
+  recommend_rice_season(R1,SEASON).
+
+ex_recommendH_harvest_date(P1,A1,T1, R1, G1, HDAY, HMONTH, HYEAR):-
+  not(is_red(P1,A1,T1,date(HYEAR,HMONTH,HDAY,0,0,0,0,-,-))).
