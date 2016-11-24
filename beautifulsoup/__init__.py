80a1696ce0a8427fa16894f88c25e6490723e80a
Kasidit Phoncharoen <kasidit.p@ku.th>
2016-11-24 09:44:51 +0700

    add rain json

--- beautifulsoup/__init__.py
+++ beautifulsoup/__init__.py
@@ -81,24 +81,40 @@ def new_way_extract_data(soup,month):
 
 
 def chiangmai():
-    present_month_url_string = 'https://www.wunderground.com/history/airport/VTCC/2016/' + str(
-        present_month) + '/21/MonthlyCalendar.html?req_city=Chiang%20Mai&req_state=&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48327&MR=1'
+    year = 2016
+    global present_month
+    new_present_month = present_month
+    present_month_url_string = 'https://www.wunderground.com/history/airport/VTCC/' +str(year)+ '/' + str(present_month) + '/21/MonthlyCalendar.html?req_city=Chiang%20Mai&req_state=&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48327&MR=1'
     present_month_page = urllib2.urlopen(present_month_url_string).read()
     present_month_soup = BeautifulSoup(present_month_page, 'html.parser')
+    new_way_extract_data(present_month_soup, MONTH[present_month])
 
-    previous_month_url_string = 'https://www.wunderground.com/history/airport/VTCC/2016/' + str(
-        present_month - 1) + '/21/MonthlyCalendar.html?req_city=Chiang%20Mai&req_state=&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48327&MR=1'
+    if int(present_month) - 1 == 0:
+        year -= 1
+        new_present_month = 13
+
+    previous_month_url_string = 'https://www.wunderground.com/history/airport/VTCC/' +str(year)+ '/' + str(new_present_month - 1) + '/21/MonthlyCalendar.html?req_city=Chiang%20Mai&req_state=&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48327&MR=1'
     previous_month_page = urllib2.urlopen(previous_month_url_string).read()
     previous_month_soup = BeautifulSoup(previous_month_page, 'html.parser')
+    new_way_extract_data(previous_month_soup, MONTH[new_present_month - 1])
 
-    second_previous_month_url_string = 'https://www.wunderground.com/history/airport/VTCC/2016/' + str(
-        present_month - 2) + '/21/MonthlyCalendar.html?req_city=Chiang%20Mai&req_state=&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48327&MR=1'
+    if int(present_month) - 2 == 0:
+        year -= 1
+        new_present_month = 14
+
+    second_previous_month_url_string = 'https://www.wunderground.com/history/airport/VTCC/' +str(year)+ '/' + str(new_present_month - 2) + '/21/MonthlyCalendar.html?req_city=Chiang%20Mai&req_state=&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48327&MR=1'
     second_previous_month_page = urllib2.urlopen(second_previous_month_url_string).read()
     second_previous_month_soup = BeautifulSoup(second_previous_month_page, 'html.parser')
+    new_way_extract_data(second_previous_month_soup, MONTH[new_present_month - 2])
 
-    new_way_extract_data(present_month_soup, MONTH[present_month])
-    new_way_extract_data(previous_month_soup, MONTH[present_month - 1])
-    new_way_extract_data(second_previous_month_soup, MONTH[present_month - 2])
+    if int(present_month) - 3 == 0:
+        year -= 1
+        new_present_month = 15
+
+    third_previous_month_url_string = 'https://www.wunderground.com/history/airport/VTCC/' +str(year)+ '/' + str(new_present_month - 3) + '/21/MonthlyCalendar.html?req_city=Chiang%20Mai&req_state=&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48327&MR=1'
+    third_previous_month_page = urllib2.urlopen(second_previous_month_url_string).read()
+    third_previous_month_soup = BeautifulSoup(second_previous_month_page, 'html.parser')
+    new_way_extract_data(third_previous_month_soup, MONTH[present_month - 3])
 
 def ubonratchathani():
     present_month_url_string = 'https://www.wunderground.com/history/airport/VTUU/2016/'+str(present_month)+'/21/MonthlyCalendar.html?req_city=Ubon%20Ratchathani&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48407#calendar'
@@ -148,16 +164,36 @@ def suphanburi():
     second_previous_month_page = urllib2.urlopen(second_previous_month_url_string).read()
     second_previous_month_soup = BeautifulSoup(second_previous_month_page, 'html.parser')
 
+    third_previous_month_url_string = 'https://www.wunderground.com/history/airport/VTBK/2016/'+str(present_month-3)+'/21/MonthlyCalendar.html?req_city=Suphan%20Buri&req_statename=Thailand&reqdb.zip=00000&reqdb.magic=1&reqdb.wmo=48425#calendar'
+    third_previous_month_page = urllib2.urlopen(second_previous_month_url_string).read()
+    third_previous_month_soup = BeautifulSoup(second_previous_month_page, 'html.parser')
+    
     new_way_extract_data(present_month_soup, MONTH[present_month])
     new_way_extract_data(previous_month_soup, MONTH[present_month - 1])
     new_way_extract_data(second_previous_month_soup, MONTH[present_month - 2])
+    new_way_extract_data(third_previous_month_soup, MONTH[present_month - 3])
 
 def print_result():
-    print "Month " + str(present_month) + " is : %s" % MONTH[present_month]
+    # print "Month " + str(present_month) + " is : %s" % MONTH[present_month]
     # print "Month " + str(present_month - 1) + " is : %s" % MONTH[present_month - 1]
     # print "Month " + str(present_month - 2) + " is : %s" % MONTH[present_month - 2]
+    # print "Month " + str(present_month - 3) + " is : %s" % MONTH[present_month - 2]
     # print type(MONTH[present_month])
-    print json.loads(str(MONTH[present_month]).replace("'","\""))
+    # print json.loads(str(MONTH[present_month]).replace("'","\""))
+    get_result()
+
+def get_result():
+    check_rain(present_month)
+    check_rain(present_month-1)
+    check_rain(present_month-2)
+    check_rain(present_month-3)
+
+def check_rain(month):
+    count = 0
+    for rain in MONTH[month]:
+        if float(MONTH[month][rain]) > 1.0 :
+            count+=1
+    print str(month) + ' ' + str(count)
 
 def choose_province(province) :
     if province == 'Chiangmai':
@@ -170,8 +206,16 @@ def choose_province(province) :
         suphanburi()
     print_result()
 
+input_province = raw_input('Please input province\n')
+input_month = input('Please input Month \n')
 
-choose_province('Chiangmai')
+# input_month = int(input_month)
+# if input_month == 0:
+#     present_month = present_month
+# else:
+#     present_month = input_month
+print('Please wait . . .')
+choose_province(str(input_province))
 
 
 
