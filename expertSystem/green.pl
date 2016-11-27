43256d444edc6c10f3bfdabb020b5858e459de5e
Peaky <peak_sasuke@hotmail.com>
2016-11-27 18:18:28 +0700

    refactor fact to reserve the green and red

--- expertSystem/green.pl
+++ expertSystem/green.pl
@@ -1,6 +1,6 @@
 - module(green, []).
 
-green("chiang_mai",date(2016,11,17),date(2016,12,28)).
-green("suphanburi",date(2016,11,17),date(2016,12,28)).
-green("surin",date(2016,11,17),date(2016,12,28)).
-green("ubon_ratchathani",date(2016,11,17),date(2016,12,28)).
+green("chiang_mai",_,_,date(2016,11,17),date(2016,12,28)).
+green("suphanburi",_,_,date(2016,11,17),date(2016,12,28)).
+green("surin",_,_,date(2016,11,17),date(2016,12,28)).
+green("ubon_ratchathani",_,_,date(2016,11,17),date(2016,12,28)).
